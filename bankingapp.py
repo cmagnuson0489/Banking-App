6793d58cb9673d1b219c389993410d8587092a78
Christopher Magnuson <cmagnuson0489@gmail.com>
2022-06-09 10:29:57 -0700

    Additional changes to banking app

--- bankingapp.py
+++ bankingapp.py
@@ -1,6 +1,10 @@
-class bankAccount:
+import random
+# Generate 5 random numbers between 1 and 10
+randomlist = random.sample(range(1, 10), 6)
+accountNumber = ''.join(str(randomlist).split(','))
 
-    accountNumber = 0
+
+class bankAccount:
 
     customerName = ""
 
@@ -10,15 +14,29 @@ class bankAccount:
 
     def createAccount(self):
 
-        self.accountNumber = int(input("Please enter your account number:"))
-
         self.name = input(
             "Please enter the account holder's first and last name:")
 
         self.type = input(
-            "Please enter the account type such as Checking or Savings: ")
+            "Please enter the account you would like to access such as Checking, Savings, Money Market: ")
 
         self.customerDeposit = int(input(
-            "Please enter the initial amount you would like to deposit:"))
+            "Please enter the amount you would like to deposit in your account:"))
 
-        print("\n Congratulations your account has been created")
+        print("\n Congratulations! Your account has been created")
+
+    def displayAccount(self):
+
+        print("Account Holder's Name: ", self.name)
+
+        print("Type of Account:", self.type)
+
+        print("Balance:", self.deposit)
+
+
+def modifyAccount(self):
+
+    self.name = input("Modify the Account Holder's name: ")
+
+    self.type = input(
+        "Modify the type of Account(checking, savings, money market): ")
